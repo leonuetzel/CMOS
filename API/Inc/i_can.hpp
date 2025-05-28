76f560b4836ff04524e38f69c69283935bfceab8
Leopold Nützel <leopold_nuetzel@web.de>
2025-05-28 21:56:39 +0200

    Additional CAN Errors added

--- API/Inc/i_can.hpp
+++ API/Inc/i_can.hpp
@@ -13,17 +13,27 @@ class I_CAN
 		
 		enum class e_error: uint8
 		{
-			STUFFING									= 0x1,
-			FORM											= 0x2,
-			ACK												= 0x3,
-			BIT_RECESSIVE							= 0x4,
-			BIT_DOMINANT							= 0x5,
-			CRC												= 0x6,
-			SET_BY_SOFTWARE						= 0x7,
+			STUFFING															= 0x1,
+			FORM																	= 0x2,
+			ACK																		= 0x3,
+			BIT_RECESSIVE													= 0x4,
+			BIT_DOMINANT													= 0x5,
+			CRC																		= 0x6,
+			SET_BY_SOFTWARE												= 0x7,
 			
-			RX_FIFO_OVERFLOW					= 0xFD,
-			TX_RINGBUFFER_OVERFLOW		= 0xFE,
-			RX_RINGBUFFER_OVERFLOW		= 0xFF
+			ACCESS_TO_RESERVED_AREA								= 0xF0,
+			PROTOCOL_ERROR_IN_DATA_PHASE					= 0xF1,
+			PROTOCOL_ERROR_IN_ARBITRATION_PHASE		= 0xF2,
+			WATCHDOG_INTERRUPT										= 0xF3,
+			ERROR_LOGGING_OVERFLOW								= 0xF4,
+			TIMEOUT																= 0xF5,
+			MESSAGE_RAM_ACCESS_FAILURE						= 0xF6,
+			TX_EVENT_FIFO_ELEMENT_LOST						= 0xF7,
+			
+			
+			RX_FIFO_OVERFLOW											= 0xFD,
+			TX_RINGBUFFER_OVERFLOW								= 0xFE,
+			RX_RINGBUFFER_OVERFLOW								= 0xFF
 		};
 		
 		enum class e_state: uint8
